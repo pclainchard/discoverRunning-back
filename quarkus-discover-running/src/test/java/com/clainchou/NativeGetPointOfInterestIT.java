f40ffa03a5066c04a8f173b3e5f428efa02a47a0
Pierre <pierre.clainchard@kanoma.fr>
2021-07-04 22:27:28 +0200

    First Quarkus commit

---
+++ quarkus-discover-running/src/test/java/com/clainchou/NativeGetPointOfInterestIT.java
@@ -0,0 +1,9 @@
+package com.clainchou;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGetPointOfInterestIT extends GetPointOfInterestTest {
+
+    // Execute the same tests but in native mode.
+}
